2c24d325248730d19957945d4a9c6fb106754f25
Lucas-Fabri <dev.cunhaww@gmail.com>
2026-04-27 23:45:58 -0300

    feat: new environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
+// Define o schema para variáveis.
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  DATABASE_URL: z.string().min(1),
+  JWT_SECRET: z.string().min(16),
+  JWT_EXPIRES_IN: z.string().default('7d'),
 })
 
 export const env = envSchema.parse(process.env)
